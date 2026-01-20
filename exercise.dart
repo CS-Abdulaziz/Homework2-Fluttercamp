e8ea6c2152da198caafda18d74db7ca0088cc734
CS-Abdulaziz <ctrl.abdulaziz@gmail.com>
2026-01-20 22:04:20 +0300

    first commit

---
+++ exercise.dart
@@ -0,0 +1,57 @@
+class Person{
+  String? name;
+  String? email;
+
+  Person(this.name, this.email);
+}
+
+class Author extends Person{
+  static int id = 0;
+  int? authID;
+  Author(super.name, super.email){
+    authID = ++id;
+  }
+
+  void printAuthorInfo(){
+    print('Author name: $name, Email: $email');
+  }
+}
+
+class Book{
+
+  String? name;
+  String? isbn;
+  int? editionNumber;
+  Author? author;
+
+  Book({this.name, this.isbn, this.author, this.editionNumber});
+
+  void printBook(){
+      print('Book name: $name, ISBN: $isbn, editionNumber: $editionNumber');
+  }
+
+}
+
+class Customer extends Person{
+
+  List<Book> bookList = [];
+  Customer(super.name, super.email);
+
+  void addBook(Book book){
+
+    bookList.add(book);
+  }
+
+  void printInformation(){
+    print('Name: $name, Email: $email');
+    print('-' * 100);
+    
+    for (var book in bookList!) {
+      book.printBook();
+      book.author!.printAuthorInfo();
+      print('-' * 100);
+    }
+  }
+}
+
+
---
+++ main.dart
@@ -0,0 +1,23 @@
+import 'exercise.dart'; // I thought there is a trick here :)
+
+void main(){
+
+  Author author1 = Author('Ahmed', 'Ahmed@gmail.com');
+  Author author2 = Author('Ali', 'Ali@gmail.com');
+  Author author3 = Author('Azoz', 'azoz@gmail.com');
+
+  Book book1 = Book(name: 'OOP', isbn: '1234', author: author1, editionNumber: 1);
+  Book book2 = Book(name: 'DAA', isbn: '1114', author: author2, editionNumber: 2);
+  Book book3 = Book(name: 'Data science', isbn: '4444', author: author3, editionNumber: 3);
+
+  print(author1.authID);
+  print(author2.authID);
+  print(author3.authID);
+
+  Customer customer1 = Customer('Mohammed', 'Mohammed123@gmail.com');
+  customer1.addBook(book1);
+  customer1.addBook(book2);
+  customer1.addBook(book3);
+
+  customer1.printInformation();
+}
